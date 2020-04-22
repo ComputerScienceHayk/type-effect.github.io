850a4025b28b420c7225dfa3214cc6123a0575ed
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-04-22 18:35:01 +0400

    Updates

---
+++ precache-manifest.5fa8c02a3791e7dc6c06437d3ebe111c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4fe7ee91f54069afb797b228528472",
+    "url": "/type-effect/index.html"
+  },
+  {
+    "revision": "e8439a71c2ab31f0206c",
+    "url": "/type-effect/static/css/main.93b18ae8.chunk.css"
+  },
+  {
+    "revision": "438778e41349e8043c53",
+    "url": "/type-effect/static/js/2.d0e88bf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-effect/static/js/2.d0e88bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8439a71c2ab31f0206c",
+    "url": "/type-effect/static/js/main.4e210dc6.chunk.js"
+  },
+  {
+    "revision": "54757e97ce5e82eb0d53",
+    "url": "/type-effect/static/js/runtime-main.2228472f.js"
+  },
+  {
+    "revision": "22256a70e83d01b3e60769871876f204",
+    "url": "/type-effect/static/media/back.22256a70.jpg"
+  }
+]);
